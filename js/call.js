b73b5a63ce5ab07f6d027fc9c8ceb92309e544c1
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-04-24 22:36:36 +0530

    added API Call

---
+++ js/call.js
@@ -0,0 +1,24 @@
+async function submitForm() {
+    var code = document.getElementById('code').value;
+    // var postData = {
+    //     code: code
+    // };
+
+    const res = await fetch('http://127.0.0.1:8000/getcodereview', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({code})
+    });
+
+    const data = await res.json();
+
+    dataOutput.innerText = data.output;
+
+}
+
+const submitButton = document.getElementById('submitButt');
+const dataOutput = document.getElementById('output');
+
+submitButton.addEventListener('click',submitForm)
